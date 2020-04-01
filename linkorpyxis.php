c86598d7bcad326ae52e321a1e8f5488cd910fdd
Eman Curmi <emancurmi@gmail.com>
2020-04-01 00:26:07 -0400

    Create linkorpyxis.php

---
+++ linkorpyxis.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Link Orpyxis
+Description: Link website to orpyxis for code management and security
+Version: 4.2
+License: GPL
+Author: Eman Curmi
+Author URL: https://www.orpyxis.com
+*/
+
+// remove unnecessary menus from WordPress Dashboard 
+function remove_admin_menus () { 
+    global $menu; 
+    $removed = array( 
+        __('LMS'), 
+		__('WPLMS'),
+        __('Font Awesome') 
+	); 
+    end ($menu); 
+    while (prev($menu)){ 
+        $value = explode(
+                ' ',
+                $menu[key($menu)][0]); 
+        if(in_array($value[0] != NULL?$value[0]:"" , $removed)){
+            unset($menu[key($menu)]);
+        }
+    } 
+} 
+add_action('admin_menu', 'remove_admin_menus');
+
+?>
